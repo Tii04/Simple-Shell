62e882190479230f7b3b8a936f393b968b09c825
Tiisetso <tiisetso.qsebata@gmail.com>
2022-09-05 07:45:40 -0700

    formatting

--- string1.c
+++ string1.c
@@ -4,9 +4,9 @@
  * _strcpy - copies a string
  * @dest: the destination
  * @src: the source
- *
  * Return: pointer to destination
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -25,9 +25,9 @@ char *_strcpy(char *dest, char *src)
 /**
  * _strdup - duplicates a string
  * @str: the string to duplicate
- *
  * Return: pointer to the duplicated string
  */
+
 char *_strdup(const char *str)
 {
 	int length = 0;
@@ -48,9 +48,9 @@ char *_strdup(const char *str)
 /**
  *_puts - prints an input string
  *@str: the string to be printed
- *
  * Return: Nothing
  */
+
 void _puts(char *str)
 {
 	int i = 0;
@@ -67,10 +67,10 @@ void _puts(char *str)
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	static int i;
